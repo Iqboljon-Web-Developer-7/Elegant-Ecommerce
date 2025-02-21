fa2fc326a378c45b9bc710ee04b2773c23369bdb
Iqboljon-Web-Developer-7 <iqboljonummataliyev30@gmail.com>
2025-02-21 14:56:33 +0500

    Refactor SimpleHeading component to use h1 tag for improved semantic structure

--- src/components/Home/SimpleHeading/SimpleHeading.tsx
+++ src/components/Home/SimpleHeading/SimpleHeading.tsx
@@ -1,10 +1,10 @@
 export default function SimpleHeading() {
   return (
     <div className="simpleHeadingContainer">
-      <h2 className="simpleHeadingH2">
+      <h1 className="simpleHeadingH2">
         Simply Unique<span className="text-neutral-400">/</span> Simply Better
         <span className="text-neutral-400">.</span>
-      </h2>
+      </h1>
       <p className="simpleHeadingP">
         <strong className="text-black">3legant</strong> is a gift & decorations
         store based in HCMC, Vietnam. Est since 2019.{" "}
