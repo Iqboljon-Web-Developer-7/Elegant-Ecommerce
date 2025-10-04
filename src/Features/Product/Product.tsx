2cb97de2df32bf710a4e21f41ae70c8c6f753e2c
Iqboljon Ummataliyev <iqboljonummataliyev30@gmail.com>
2025-10-04 13:52:21 +0500

    Update Product.tsx

--- src/Features/Product/Product.tsx
+++ src/Features/Product/Product.tsx
@@ -25,7 +25,7 @@ import {
 } from "@/components/ui/dialog"
 import { ToastAction } from "@/components/ui/toast";
 import { useCreateReview, useDeleteReview, useProductReviews } from "./hooks/useReviews";
-import { Star, Trash2 } from "lucide-react";
+import { Star } from "lucide-react";
 import { StarRating } from "./_components/StarRating";
 
 const Product: FC = () => {
@@ -159,7 +159,7 @@ const Product: FC = () => {
                     <p className="font-semibold">{r.postedBy.name}</p>
                     {r.postedBy._id === userInfo?._id && (
                       <button onClick={() => deleteReview.mutate(r._id)} aria-label="Delete review">
-                        <Trash2 className="text-red-500" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                       </button>
                     )}
                   </div>
